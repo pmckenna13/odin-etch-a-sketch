24466e27d3c5a429025774ba74ab9ccae1152919
pmckenna13 <patrickmckennaeng@gmail.comgit config --global user.emaail patrickmckennaeng@gmail.com>
2022-09-19 21:04:22 -0400

    Finish 1st drafrt of project

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const gridContainer = document.getElementById("gridContainer");
 const sizePicker = document.getElementById("sizePicker");
-const gridSize = sizePicker.value;
-sizePicker.addEventListener('change', resetGrid);
+const colorPicker = document.getElementById("colorPicker");
+const clearBtn = document.getElementById("clearBtn");
+const defaultSize = 16
+
+sizePicker.value = defaultSize;
+colorPicker.value = "black";
+sizePicker.addEventListener('change', resizeGrid);
+clearBtn.addEventListener("click", resetGrid);
 
 
 var mouseDown = "false";
@@ -12,15 +18,31 @@ document.body.onmouseup = function(){
     mouseDown = "false";
 }
 
+function resizeGrid(e)
+{
+    const newSize = e.target.value;
+    if(newSize > 0 && newSize <= 100)
+    {
+        gridContainer.innerHTML="";
+        createGrid(e.target.value);
+    }
+    else{
+        alert("Enter a number between 1 and 100!!!");
+    }
+}
+
 function resetGrid(e)
 {
-    gridContainer.innerHTML="";
-    createGrid(e.target.value);
+    const gridCells = document.querySelectorAll(".gridCell");
+    gridCells.forEach(cell => cell.style.backgroundColor = "white");
 }
 
 function changeColor(e){
-    const newColor = document.getElementById("colorPicker").value;
-    e.target.style.backgroundColor = newColor;
+    if(mouseDown == "true")
+    {
+        const newColor = document.getElementById("colorPicker").value;
+        e.target.style.backgroundColor = newColor;
+    }
 }
 
 function createGrid(size)
@@ -31,9 +53,10 @@ function createGrid(size)
     {
         let cell = document.createElement("div");
         cell.addEventListener("mouseover", changeColor);
+        cell.style.setProperty("user-select", "none");
         gridContainer.appendChild(cell).className = "gridCell";
     }
 }
 
-createGrid(gridSize);
+createGrid(defaultSize);
 
